3917aaebc703c33d2898a28b7840ee41bc037ab0
swaraj shinde <swaraj.shinde@shivrai.co.in>
2026-02-25 10:51:03 +0530

    test

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,7 +1,9 @@
-import yaml 
-import os 
+import yaml
+import os
 
-def load_config(config_path : str = "config/config.yaml"):
-    with open(config_path , "r") as file:
-        config = yaml.safe_load(file)
-    return config
+def load_config(config_dir: str = r"F:\Git_gen_ai_project\calculator-agentic-AI-project\config"):
+    config_path = os.path.join(config_dir, "config.yaml")
+    config_path = os.path.abspath(config_path)
+
+    with open(config_path, "r") as file:
+        return yaml.safe_load(file)
